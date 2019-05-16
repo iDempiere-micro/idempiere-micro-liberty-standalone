ae51871eb9f20b8f7c400c8b6df5cf7962f36020
David Podhola <david@podhola.net>
2019-05-16 20:58:34 +0200

    code comments

--- src/main/kotlin/company/bigger/idempiere/resolver/QueryResolver.kt
+++ src/main/kotlin/company/bigger/idempiere/resolver/QueryResolver.kt
@@ -14,18 +14,59 @@ class QueryResolver : BaseResolver(), GraphQLQueryResolver {
         inject(globalContext.module)
     }
 
+    /**
+     * APIversion
+     */
     val version = Version(VER)
+    /**
+     * Current user
+     */
     val me get() = authenticationService.currentUser()
+    /**
+     * Users
+     */
     val users get() = usersService.getUsers()
+    /**
+     * Business partners
+     */
     val businessPartners get() = businessPartnerService.getAll()
+    /**
+     * Currencies
+     */
     val currencies get() = currencyService.getAll()
+    /**
+     * countries
+     */
     val countries get() = countryService.getAll()
+    /**
+     * categories
+     */
     val categories get() = categoryService.getAll()
+    /**
+     * sales Orders
+     */
     val salesOrders get() = salesOrderService.getAll()
+    /**
+     * products
+     */
     val products get() = productService.getAll()
+    /**
+     * Contact activities
+     */
     val contactActivities get() = contactActivityService.getAll()
 
+    /**
+     * Sales order by Id
+     */
     fun salesOrder(id: Int) = salesOrderService.getById(id)
+
+    /**
+     * business partner by id
+     */
     fun businessPartner(id: Int) = businessPartnerService.getById(id)
+
+    /**
+     * product by id
+     */
     fun product(id: Int) = productService.getById(id)
 }
